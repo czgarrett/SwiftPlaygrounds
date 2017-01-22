4f93c953f40880bb1d1ce85e2eb0c3af2c1b06ab
walshyb <bmwalshy@gmail.com>
2017-01-22 09:55:29 -0500

    start Chapter 2: Basic Operators
    Add examples of assignment operator, arithmetic operators, concatenation, remainder, unary minus/plus

---
+++ Playgrounds/ByTheBook/02-BasicOperators.playground/Contents.swift
@@ -0,0 +1,34 @@
+// Chapter 2: Basic Operators
+
+// Assignment Operator: =
+var applesInBasket = 5
+applesInBasket = 4
+
+applesInBasket // now equals 4
+
+let (pointA, pointB) = (3.5, 5.2)
+
+
+// Arithmetic Operators
+2 + 2   // equals 4 (not 🐟)
+3 - 2   // equals 1
+10 / 5  // equals 2
+5 * 5   // equals 25
+
+// Concatenation
+let foobar = "foo, " + "bar" // equals "foo, bar"
+
+// Remainder Operator
+var remainder = 10 % 6   // equals 4 because 6 divides into 10 once
+                         // and leaves us with a remainder of 4
+
+// Unary Minus Operator
+var forty = 40
+var negativeForty = -forty // equivalent of assigning var to be -40 (negative 40)
+
+// Unary Plus Operator
+var minusSeven = -7
+var stillMinusSeven = +minusSeven
+
+// this operator actually doesn't do anything
+// but can provide symmetry to code
